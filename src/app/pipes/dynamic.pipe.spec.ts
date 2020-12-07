9231a084dcdcbf2868c5eab4f9a4801b27cb212c
Diego Romario <diego.romario@outlook.com>
2020-12-07 20:36:28 -0300

    Adicionando teste para formatação de moeda

--- src/app/pipes/dynamic.pipe.spec.ts
+++ src/app/pipes/dynamic.pipe.spec.ts
@@ -1,13 +1,27 @@
-import { CurrencyPipe, DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { DynamicPipe } from './dynamic.pipe';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 describe('Dynamic pipe', () => {
     let pipe: DynamicPipe;
     beforeEach(() => {
-        pipe = new DynamicPipe(new DatePipe(''), new CurrencyPipe(''));
+        TestBed.configureCompiler({
+        });
+        pipe = new DynamicPipe(new DatePipe('pt-BR'), new CurrencyPipe('pt-BR'));
     });
     it('Dado nova instancia o valor não deve ser nulo ou indefinido', () => {
         expect(pipe).toBeTruthy();
     });
+
+    it('Dado valor número deve formatar para moeda', () => {
+        const valorFormatado = pipe.transform('1000', 'currency');
+        console.log(valorFormatado);
+        expect(valorFormatado).toContain('1.000,00');
+    });
+
+    
 
 });
