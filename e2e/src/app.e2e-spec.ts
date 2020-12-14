744b842b4ddd4fdcc597f1cc370319e6680cf5fa
Diego Romario <diego.romario@outlook.com>
2020-12-14 20:07:03 -0300

    Adicionando primeiro teste e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('deve apresentar mensagem de boas viandas', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('magic-formula-webapp app is running!');
+    expect(page.getTitleText()).toEqual('Welcome!');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.id('welcome')).getText() as Promise<string>;
   }
 }
